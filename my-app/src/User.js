de6d053057f73d20152351f5f98b981321d1f46e
Yashodhan2001 <89022164+Yashodhan2001@users.noreply.github.com>
2022-07-18 16:12:46 +0530

    This is for Usr.js

--- my-app/src/User.js
+++ my-app/src/User.js
@@ -5,6 +5,7 @@ function User()
             <h1>Yashodhan Vats</h1>
             <h2>SoftWare Developer</h2>
             <p><b>I am passionate for my Work</b></p>
+            <h3>Rampuri Muzaffarnagar</h3>
         </div>
     )
 }
