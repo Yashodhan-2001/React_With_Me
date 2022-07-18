a48db929ea2ce05496f301f3691ae2591c8a1d40
Yashodhan2001 <89022164+Yashodhan2001@users.noreply.github.com>
2022-07-18 16:47:47 +0530

    Changes of Class  Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import User from'./User';
+import UserClass from './UserClass';
 
 function App() {
   return (
     <div className="App">
       <User/>
+      <UserClass/>
     </div>
   );
 }
