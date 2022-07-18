ae09c461595dda8f94ac5d2f028d00d13ae95c48
Yashodhan2001 <89022164+Yashodhan2001@users.noreply.github.com>
2022-07-18 16:03:34 +0530

    This is pubic folder of react project

---
+++ my-app/src/User.js
@@ -0,0 +1,11 @@
+function User()
+{
+    return(
+        <div>
+            <h1>Yashodhan Vats</h1>
+            <h2>SoftWare Developer</h2>
+            <p><b>I am passionate for my Work</b></p>
+        </div>
+    )
+}
+export default User;
