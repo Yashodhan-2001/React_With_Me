6169330dc1722687094a3799f4ca46531c98e6d3
Yashodhan2001 <89022164+Yashodhan2001@users.noreply.github.com>
2022-08-01 11:55:08 +0530

    Event file pushing

---
+++ my-app/src/Event.js
@@ -0,0 +1,14 @@
+function Event()
+{
+    function Add()
+    {
+        alert(3+4);
+    }
+    return(
+        <div>
+            <button onClick={Add}>Click Bro</button>
+
+        </div>
+    )
+}
+export default Event;
